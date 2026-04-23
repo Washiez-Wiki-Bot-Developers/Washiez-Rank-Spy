ad11e61e639eb0d972474a51963e58137caafb78
MrT <MrT-tak@outlook.com>
2026-04-23 01:20:05 +0100

    Update app.py to include ranks.
    Updates to add "Assistant Director" as well as making sure "Head Corporate" is registered.

--- app.py
+++ app.py
@@ -66,8 +66,8 @@ LOW_RANKS = [
     "Head Operator",
 ]
 MID_RANKS = ["Shift Leader", "Supervisor", "Assistant Manager", "General Manager"]
-MGMT_RANKS = ["Junior Director", "Senior Director", "Head Director"]
-CORP_RANKS = ["Corporate Intern", "Junior Corporate", "Senior Corporate"]
+MGMT_RANKS = ["Assistant Director", "Junior Director", "Senior Director", "Head Director"]
+CORP_RANKS = ["Corporate Intern", "Junior Corporate", "Senior Corporate", "Head Corporate"]
 LS_RANKS = [
     "Chief Human Resources Officer",
     "Chief Public Relations Officer",
